a028c910081abd65b267bfe55a7745e39e620abf
Carlos Torres <cftorres20@gmail.com>
2025-09-19 18:34:12 -0300

    Se realiza clase TrampasLog TrampasEngine y Dado

---
+++ Primer_Instancia_Evaluativa/src/primer_instancia_evaluativa/Dado.java
@@ -0,0 +1,14 @@
+package primer_instancia_evaluativa;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author carlo
+ */
+public class Dado {
+    
+}
